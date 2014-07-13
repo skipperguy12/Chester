a5bcd11800f5cd64a3ca1a141275f2ca99013526
gcflames5 <gcflames5@gmail.com>
2014-07-13 13:35:14 -0400

    Remove more unused fields from Menu

--- src/main/java/com/github/plastix/chester/gui/Menu.java
+++ src/main/java/com/github/plastix/chester/gui/Menu.java
@@ -5,28 +5,12 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class Menu {
-    private final static ItemStack DEFAULT_EMPTY_SLOT = new ItemStack(119, 1);
-
     protected Inventory inv;
-    protected final ItemStack emptySlot;
     protected MenuManager manager;
-
-    public Menu(MenuManager manager, Inventory inv, ItemStack emptySlot) {
-        this.manager = manager;
-        this.inv = inv;
-        this.emptySlot = emptySlot;
-    }
 
     public Menu(MenuManager manager, Inventory inv) {
         this.manager = manager;
         this.inv = inv;
-        this.emptySlot = DEFAULT_EMPTY_SLOT;
-    }
-
-    public Menu(MenuManager manager, Inventory inv, Material emptySlot){
-        this.manager = manager;
-        this.inv = inv;
-        this.emptySlot = new ItemStack(emptySlot);
     }
 
     public void setInventory(Inventory inv) {
@@ -35,9 +19,5 @@ public class Menu {
 
     public Inventory getInventory() {
         return this.inv;
-    }
-
-    public ItemStack getEmptySlot() {
-        return emptySlot;
     }
 }
