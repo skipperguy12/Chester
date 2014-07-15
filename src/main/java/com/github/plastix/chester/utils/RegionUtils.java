6542a93b2a1aa509b6d3fd6bdd186c7b0ffee0fa
skipperguy12 <skipperguy12@users.noreply.github.com>
2014-07-15 15:32:36 -0400

    Make RegionUtils.java methods static

--- src/main/java/com/github/plastix/chester/utils/RegionUtils.java
+++ src/main/java/com/github/plastix/chester/utils/RegionUtils.java
@@ -17,16 +17,16 @@ public class RegionUtils {
      * Loop through blocks in a cuboid region.
      *
      * @param world The world of the selection
-     * @param pos1 position 1 of the selection
-     * @param pos2 position 2 of the selection
+     * @param pos1  position 1 of the selection
+     * @param pos2  position 2 of the selection
      * @return a list of all blocks in the region
      */
-    public List<Block> getRegionBlocks(World world, Location pos1, Location pos2) {
+    public static List<Block> getRegionBlocks(World world, Location pos1, Location pos2) {
         List<Block> blocks = new ArrayList<Block>();
 
-        for(double x = pos1.getX(); x <= pos2.getX(); x++) {
-            for(double y = pos1.getY(); y <= pos2.getY(); y++) {
-                for(double z = pos1.getZ(); z <= pos2.getZ(); z++) {
+        for (double x = pos1.getX(); x <= pos2.getX(); x++) {
+            for (double y = pos1.getY(); y <= pos2.getY(); y++) {
+                for (double z = pos1.getZ(); z <= pos2.getZ(); z++) {
                     Location loc = new Location(world, x, y, z);
                     blocks.add(loc.getBlock());
                 }
@@ -40,12 +40,12 @@ public class RegionUtils {
      * Find all blocks of certain type in a cuboid region.
      *
      * @param world The world of the selection
-     * @param pos1 position 1 of the selection
-     * @param pos2 position 2 of the selection
+     * @param pos1  position 1 of the selection
+     * @param pos2  position 2 of the selection
      * @param query the material to find in the region
      * @return a list of every block that matches the query in the region
      */
-    public List<Block> findBlocksInRegion(World world, Location pos1, Location pos2, Material query) {
+    public static List<Block> findBlocksInRegion(World world, Location pos1, Location pos2, Material query) {
         List<Block> blocks = getRegionBlocks(world, pos1, pos2);
 
         List<Material> queryList = new ArrayList<Material>();
@@ -58,12 +58,12 @@ public class RegionUtils {
      * Find all blocks of certain type in a cuboid region.
      *
      * @param world The world of the selection
-     * @param pos1 position 1 of the selection
-     * @param pos2 position 2 of the selection
+     * @param pos1  position 1 of the selection
+     * @param pos2  position 2 of the selection
      * @param query the list of materials to find in the region
      * @return a list of every block that matches the query in the region
      */
-    public List<Block> findBlocksInRegion(World world, Location pos1, Location pos2, List<Material> query) {
+    public static List<Block> findBlocksInRegion(World world, Location pos1, Location pos2, List<Material> query) {
         List<Block> blocks = getRegionBlocks(world, pos1, pos2);
         return findBlocksInList(blocks, query);
     }
@@ -72,10 +72,10 @@ public class RegionUtils {
      * Find all matching blocks in a list.
      *
      * @param search The list to find the blocks in
-     * @param query The types of blocks to find
+     * @param query  The types of blocks to find
      * @return a list of every block that matches the query
      */
-    public List<Block> findBlocksInList(List<Block> search, List<Material> query) {
+    public static List<Block> findBlocksInList(List<Block> search, List<Material> query) {
         List<Block> results = new ArrayList<Block>();
 
         for (Block block : search) {
